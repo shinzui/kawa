fe2ab6ed47e1a8f065249aff4effecac2660dc84
Nadeem Bitar <nadeem@gmail.com>
2012-01-21 02:44:07 -0800

    Display page timestamp and layout cleanup

--- app/presenters/page_presenter.rb
+++ app/presenters/page_presenter.rb
@@ -1,4 +1,6 @@
 class PagePresenter
+  include Kawa::Common::Presenter
+
   delegate :name, :to  => :@model
   attr_reader :model
 
@@ -17,5 +19,18 @@ class PagePresenter
   def data
     PageRenderer.new(@model).render
   end
+
+  def created_at
+    return unless @model.created_at
+    h.content_tag(:strong, "Created on:") +
+    h.content_tag(:em, I18n.l(@model.created_at, :format  => :long))
+  end
+
+  def updated_at
+    return if @model.updated_at.nil? || @model.updated_at == @model.created_at
+    h.content_tag(:strong, "Last updated on:") +
+    h.content_tag(:em, I18n.l(@model.updated_at, :format  => :long))
+  end
+
 
 end
