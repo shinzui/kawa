101fdd2182503458232f24c53b0735548841e391
Nadeem Bitar <nadeem@gmail.com>
2014-01-12 18:24:08 -0800

    Remove unneeded kiqstand sidekiq middleware

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,6 @@ raise "Redis is misonfigured. url: #{redis_url} namespace: #{redis_namespace}" i
 
 Sidekiq.configure_server do |config|
   config.redis = { :url => redis_url, :namespace => redis_namespace, :size => 25 }
-  config.server_middleware do |chain|
-    chain.add Kiqstand::Middleware
-  end
 end
 
 Sidekiq.configure_client do |config|
